317d7f7fd5f8f61a8f4f62b5751eeb101d9e0953
RyanCri <N00221108@iadt.ie>
2024-12-17 17:04:50 +0000

    fix menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject StartMenu;
+    public GameObject CreditsMenu;
+    
     public void PlayGame()
     {
         SceneManager.LoadScene("Level_00_Tutorial", LoadSceneMode.Single);
+    }
+
+    public void Credits()
+    {
+        CreditsMenu.SetActive(true);
+        StartMenu.SetActive(false);
+    }
+
+    public void ExitCredits()
+    {
+        StartMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
     }
 
     public void doExitGame() {
